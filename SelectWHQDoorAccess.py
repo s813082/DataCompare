9e2dcc522f6750983d4ee80af0fdbdc20587378d
Barry Tsai <Barry_Tsai@wistron>
2020-02-12 17:27:35 +0800

    Delete SelectWHQDoorAccess.py

--- SelectWHQDoorAccess.py
+++
@@ -1,7 +0,0 @@
-import pymssql
-import AccessConfig 
-conn = pymssql.connect(AccessConfig.SERVER,AccessConfig.UID, AccessConfig.PWD, AccessConfig.DATABASE)
-cursor = conn.cursor()
-cursor.execute("select B.*, E.t_Number, E.t_Name, E.t_CreateDate from WistronMRBooking B inner join OGEmp E on B.t_PK = E.t_PK order by t_CreateDate desc")
-
-print(row)
