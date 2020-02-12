0492ecb94caa96cca08bd6f5003c69184acee6a8
Barry Tsai <Barry_Tsai@wistron>
2020-02-12 17:28:02 +0800

    Main Program

---
+++ Program.py
@@ -0,0 +1,8 @@
+import SelectWHQAccessData
+import SelectWHQEFormData
+
+print("Program start...")
+print("Get Access Data")
+SelectWHQAccessData.GetAccessData()
+print("Get EForm Data")
+SelectWHQEFormData.GetEformData()
