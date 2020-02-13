330af4241a1e913f052bdc9fc73462df0830b6c0
Barry Tsai <Barry_Tsai@wistron>
2020-02-13 17:36:05 +0800

    Add QAS

--- EformConfig.py
+++ EformConfig.py
@@ -1,4 +1,11 @@
-SERVER="VirtualSQL"
+# QAS
+SERVER="TWTPESQL07"
 DATABASE="WHQMeetingRoom2"
 UID="awadmin"
 PWD="2wsx3edc@WHQ"
+
+# PRD
+# SERVER="VirtualSQL"
+# DATABASE="WHQMeetingRoom2"
+# UID="awadmin"
+# PWD="2wsx3edc@WHQ"
