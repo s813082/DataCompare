05ae6ef6d7c58a23bcd96a15927b0663accb6a99
Barry Tsai <Barry_Tsai@wistron>
2020-02-19 17:05:52 +0800

    更改檔案路徑

--- LogtoFile.py
+++ LogtoFile.py
@@ -3,8 +3,8 @@ from datetime import date
 
 FORMAT = '%(asctime)s %(levelname)s: %(message)s'
 DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
-# FileName = 'log/'+str(date.today())+'.log'
-FileName = str(date.today())
+FileName = 'log/'+str(date.today())+'.log'
+# FileName = str(date.today())
 logging.basicConfig(level=logging.DEBUG, filemode='a', filename=FileName, format=FORMAT, datefmt=DATE_FORMAT )
 
 def LoggingMSG(Messsage):
