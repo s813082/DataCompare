6802dfefb53d4fef27a47f69ad96140ed47edae1
Barry Tsai <Barry_Tsai@wistron>
2020-02-27 17:25:11 +0800

    更新文字

--- DataModel.py
+++ DataModel.py
@@ -1,5 +1,5 @@
 class BookingDetail:
-    def __init__(self,ID,Booking,Resource_ID,START_DT,END_DT,APPLY_DT):
+    def __init__(self,ID,Booking,Resource_ID,START_DT,END_DT,APPLY_DT,FullName):
         super().__init__()
         self.ID = ID
         self.Booking = Booking
@@ -7,7 +7,7 @@ class BookingDetail:
         self.START_DT = START_DT
         self.END_DT = END_DT
         self.APPLY_DT = APPLY_DT
-        self.FullName = "此時段已被借用" 
+        self.FullName = "此時段已被"+FullName+"借用" 
         self.USERID = "10612039"
         self.Review_Status = "A"
 
